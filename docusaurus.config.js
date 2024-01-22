c601de9d5e6ab606101efcb306b8bb37c9ebd902
Jeremie Zarka <jeremie.zarka@gmail.com>
2024-01-22 11:57:06 +0100

    change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,6 +14,7 @@ const config = {
 
   // Set the production url of your site here
   url: 'https://ghostnet.elevate-labs.fr',
+  
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
